31c8d29aae758cc867706aa5250657376893235f
Shyamraja <t7bhsh00@students.oamk.fi>
2020-02-17 21:55:47 +0200

    fixed inserting data into customer.cs  now it shows added customer info after inserting

--- librarymanagementsystem/Customers.cs
+++ librarymanagementsystem/Customers.cs
@@ -12,25 +12,59 @@ using System.Data.OleDb;
 namespace Librarysystem
 {
     public partial class Customers : Form
-       {
+    {
         OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Owner\Desktop\Library\Databaselibrarym.mdb");
-
+        OleDbCommand cmd;
+        OleDbDataReader dr;
+        string sql;
         public Customers()
         {
             InitializeComponent();
-     
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            con.Open();
-            OleDbCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "Insert into CustomerTable values('" + textBox1.Text + "',,'" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "')";
-            con.Close();
-            MessageBox.Show("Value inserted");
+            try
+            {
 
+                string CustomerID, Name, Phone, Age, Gender, Material;
+                CustomerID = textBox1.Text;
+                Name = textBox2.Text;
+                Phone = textBox3.Text;
+                Age = textBox4.Text;
+                Gender = textBox5.Text;
+                Material = textBox6.Text;
 
+                sql = "insert into CustomerTable (CustomerID,Name,Phone,Age,Gender,Material)values(@CustomerID,@Name,@Phone,@Age,@Gender,@Material)";
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand(sql, con);
+                cmd.Parameters.AddWithValue("@CustomerID", CustomerID);
+                cmd.Parameters.AddWithValue("@Name", Name);
+                cmd.Parameters.AddWithValue("@Phone", Phone);
+                cmd.Parameters.AddWithValue("@Age", Age);
+                cmd.Parameters.AddWithValue("@Gender", Gender);
+                cmd.Parameters.AddWithValue("@Material", Material);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Added");
+                con.Close();
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (con.State != ConnectionState.Closed) con.Close();
+            }
 
         }
 
@@ -46,7 +80,7 @@ namespace Librarysystem
             dataGridView1.DataSource = dt;
             con.Close();
         }
-       
+
 
     }
 }
