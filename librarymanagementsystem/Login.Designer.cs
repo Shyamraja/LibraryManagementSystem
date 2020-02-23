3a18e5471ea93450b8dadc4e49042c3faf69f0de
Shyamraja <t7bhsh00@students.oamk.fi>
2020-02-23 02:14:54 +0200

    Updated with login username and password

--- librarymanagementsystem/Login.Designer.cs
+++ librarymanagementsystem/Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace librarymanagementsystem
+﻿namespace Librarysystem
 {
     partial class Login
     {
@@ -28,76 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Username = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(148, 101);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(73, 17);
+            this.Username.TabIndex = 0;
+            this.Username.Text = "Username";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 40);
+            this.label1.Location = new System.Drawing.Point(148, 143);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Password";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(185, 40);
+            this.textBox1.Location = new System.Drawing.Point(268, 101);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.Size = new System.Drawing.Size(117, 22);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(185, 73);
+            this.textBox2.Location = new System.Drawing.Point(268, 143);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(190, 22);
+            this.textBox2.Size = new System.Drawing.Size(117, 22);
             this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(185, 123);
+            this.button1.Location = new System.Drawing.Point(146, 208);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Exit";
+            this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(300, 123);
+            this.button2.Location = new System.Drawing.Point(277, 208);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 31);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Login";
+            this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 230);
+            this.ClientSize = new System.Drawing.Size(747, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.Username);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -107,12 +108,11 @@
 
         #endregion
 
+        private System.Windows.Forms.Label Username;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
-
--- librarymanagementsystem/Login.cs
+++ librarymanagementsystem/Login.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace librarymanagementsystem
+namespace Librarysystem
 {
     public partial class Login : Form
     {
@@ -17,9 +17,28 @@ namespace librarymanagementsystem
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+      
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username, password;
+            username = textBox1.Text;
+            password = textBox2.Text;
+            if(username=="Admin"&& password=="Admin1290")
+            {
+                MessageBox.Show("You are logged in Successfully");    
+                Main Main = new Main();
+                Main.Show();               
+            }
+         else
+            {
+                MessageBox.Show("Username or Password is incorrect");
+            }
+        
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
