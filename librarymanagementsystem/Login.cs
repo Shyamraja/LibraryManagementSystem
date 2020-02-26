d64087bcd2ecabaa7052e2404baca0255335b101
Shyamraja <t7bhsh00@students.oamk.fi>
2020-02-26 02:03:38 +0200

    Database table for Admin added and connected with  login form only admins can access

--- librarymanagementsystem/Login.cs
+++ librarymanagementsystem/Login.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using System.Windows.Forms;
+//using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
 
 namespace Librarysystem
 {
     public partial class Login : Form
     {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Owner\Source\repos\Librarysystem\Databaselibrarym.mdb");
+
+        string sql;
         public Login()
         {
             InitializeComponent();
@@ -22,20 +30,55 @@ namespace Librarysystem
         private void button1_Click(object sender, EventArgs e)
         {
             string username, password;
-            username = textBox1.Text;
-            password = textBox2.Text;
-            if(username=="Admin"&& password=="Admin1290")
+             username = textBox1.Text;
+             password = textBox2.Text;
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Admin where username = '"+textBox1.Text+"' and password = '" + textBox2.Text+"'";
+            OleDbDataReader reader = cmd.ExecuteReader();
+            //while (reader.Read())
+                int count = 0;
+            while  (reader.Read())
             {
-                MessageBox.Show("You are logged in Successfully");
-                Librarymdiparent mdiparent = new Librarymdiparent();
-                mdiparent.Show();
-                this.Hide();
+                count = count + 1;
+
             }
-         else
+            //Checking single entry for application only one at a time
+            if (count == 1)
             {
-                MessageBox.Show("Username or Password is incorrect");
+                MessageBox.Show("You are successfully Logged in");
             }
-        
+            //
+            if (count > 1)
+            {
+                MessageBox.Show("Can't be duplicate Admin");
+            }
+
+            con.Close();
+
+            MessageBox.Show("You are logged in Successfully");
+                 Librarymdiparent mdiparent = new Librarymdiparent();
+                 mdiparent.Show();
+                 this.Hide();
+
+            
+
+            //   string username, password;
+            //   username = textBox1.Text;
+            //   password = textBox2.Text;
+            //   if(username=="Admin"&& password=="Admin1290")
+            //   {
+            //       MessageBox.Show("You are logged in Successfully");
+            //       Librarymdiparent mdiparent = new Librarymdiparent();
+            //       mdiparent.Show();
+            //       this.Hide();
+            //   }
+            //else
+            //   {
+            //       MessageBox.Show("Username or Password is incorrect");
+            //   }
+            //connection.Close();
         }
         private void button2_Click(object sender, EventArgs e)
         {
